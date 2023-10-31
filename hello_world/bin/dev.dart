411a8c6ff43517dc2a3a884747a2192daddf3f6e
santty04 <sarango844@misena.edu.co>
2023-10-31 10:35:38 -0500

    map part 3

--- hello_world/bin/dev.dart
+++ hello_world/bin/dev.dart
@@ -1,14 +1,25 @@
 void main(List<String> arguments) {
-  Map<String,String> users = {};
-  users["user1"] = 'Santiago';
-  print(users);
-  users["user2"] = 'Pepe';
-  print(users);
-  users["user1"] = 'Darwin';
-  print(users);
+  Map<int,String> products = {
+    0: 'iPod',
+    1: 'iPhone',
+    2: 'Mackbook Pro',
+    3: 'iPad'
+  };
+  print(products);
+  products[1] = "iPhone 15 pro";
+  products[5] = "Mac Air";
+  print(products);
+  // products.remove(5);
+  
+  final element = products.putIfAbsent(0, (){
+    return "Samsung Galaxy S22 Ultra";
+  });
+  print(element);
 
-  final removed = users.remove('user2');
-  print("removed $removed");
-  users.clear();
-  print(users);
+  products.removeWhere((key, value){
+    return key == 2 || key == 5;
+  });
+  print(products);
+  print(products.keys.toList());
+  print(products.values.toList());
 }
