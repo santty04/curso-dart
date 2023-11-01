d1f0815eff2679661dd755a5fa346cea78aaa1fc
santty04 <sarango844@misena.edu.co>
2023-11-01 11:43:51 -0500

    Clase if

--- hello_world/bin/dev.dart
+++ hello_world/bin/dev.dart
@@ -1,31 +1,14 @@
 void main(List<String> arguments) {
-  final user = <String, dynamic>{
-    "name": "Santiago",
-    "lastName": "Arango",
-    "age": 19,
-    "email": "arangosanti05@gmail.com",
-    "salary": 10000.99,
-    "enable": true,
-    "location": {
-      "country": "Colombia",
-      "countryCode": "CO",
-      "city": "Colombia",
-      "postalCode": 1234
-    },
-    "pets": [{
-      "name": "Max",
-      "age": 9
-    },
-    {
-      "name": "Buddy",
-      "age": 7
-    }]
-  };
 
-  print("Age: ${user['age']}");
-  print("Email: ${user['email']}");
-  final location = user['location'];
-
-  // final country = (location as Map<String, String>)['country'];
-  final List<Map<String, dynamic>> pets = user['pets'];
+  final email = "";
+  
+  if (email.contains("@")) {
+    print("is a valid email");
+  } else if(email.contains(".")) {
+    print("is a valid email");
+  } else if(email.isEmpty) {
+    print("empty email");
+  } else {
+    print("not a valid email");
+  }
 }
