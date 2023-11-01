e337eed171799f20752005765f3c26b263137baa
santty04 <sarango844@misena.edu.co>
2023-11-01 12:02:13 -0500

    Clase condicionales

--- hello_world/bin/dev.dart
+++ hello_world/bin/dev.dart
@@ -1,14 +1,24 @@
 void main(List<String> arguments) {
 
-  final email = "";
-  
-  if (email.contains("@")) {
-    print("is a valid email");
-  } else if(email.contains(".")) {
-    print("is a valid email");
-  } else if(email.isEmpty) {
-    print("empty email");
-  } else {
-    print("not a valid email");
+  // final weekDay = 6;
+  // final days = 30;
+
+  // if (weekDay >= 1 && weekDay <= 7) {
+  //   if (days >= 30 && (weekDay == 6 || weekDay == 7)) {
+  //     print("Free day");
+  //   } else {
+  //     print("😒");
+  //   }
+  // } else {
+  //   print("Invalid day of the week");
+  // }
+
+
+  final price1 = 50.99, price2 = 19.99;
+
+  final areEquals = price1 == price2;
+
+  if (true) {
+    print('True');
   }
 }
