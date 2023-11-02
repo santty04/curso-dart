7776e83c420ee36e2730623974f40b38ce702b00
santty04 <sarango844@misena.edu.co>
2023-11-02 11:33:23 -0500

    For

--- hello_world/bin/dev.dart
+++ hello_world/bin/dev.dart
@@ -1,11 +1,20 @@
 void main(List<String> arguments) {
-  
-  int index = 10;
+  final name = 'Santiago';
+  final numbers = [10, 11, 12, 13, 14, 15];
+  final products = <int, String>{
+    0: "Mac mini",
+    1: "iPhone"
+  };
 
-  do {
-    print(index);
-    index++;
-  } while (index < 100);
-  
-  print("Hello world");
+  for (final p in products.entries) {
+    print("Key: ${p.key}: Value: ${p.value}");
+  }
+
+  for (int e in numbers) {
+    print(e);
+  }
+
+  for (int index = 0; index < name.length; index++) {
+    print(name[index]);
+  }
 }
