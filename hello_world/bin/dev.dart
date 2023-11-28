fd11e48065df26145d6fcf569c6355f9b47a016f
santty04 <sarango844@misena.edu.co>
2023-11-28 11:26:02 -0500

    how function enums

--- hello_world/bin/dev.dart
+++ hello_world/bin/dev.dart
@@ -2,9 +2,23 @@ import 'package:hello_world/models/pet.dart';
 import 'package:hello_world/utils/api_key.dart';
 
 void main(List<String> arguments) {
+  print(Status.values);
 
-  Pet.updateCounter(500);
-  print(Pet.counter);
-  
+  final orderStatus = Status.created;
+
+  print(orderStatus);
 }
 
+enum Status {
+  created,
+  ready,
+  onTheWay,
+  delivered,
+}
+
+// class Status {
+//   static const int created = 0;
+//   static const int ready = 1;
+//   static const int onTheWay = 2;
+//   static const int delivered = 3;
+// }
