a86b0fa4476bd25a9cbb493a74cf8778a744e3db
santty04 <sarango844@misena.edu.co>
2023-11-30 16:18:45 -0500

    generic dates in dart

--- hello_world/bin/dev.dart
+++ hello_world/bin/dev.dart
@@ -1,34 +1,22 @@
-import 'dart:io';
-import 'package:hello_world/utils/menu.dart';
+import 'package:hello_world/models/tuple.dart';
 
 void main(List<String> arguments) {
-  final menu1 = Menu<String>(label: "Are you a software developer?", options: [
-    MenuOption(value: "yes", text: "yes"),
-    MenuOption(value: "no", text: "no")
-  ]);
-
-  final selectedOption = menu1.choose();
-
-  switch (selectedOption) {
-    case "yes":
-      print("😁");
-      final menu2 = Menu<int>(label: "What is your favorite?", options: [
-        MenuOption(value: 1, text: "Mobile"),
-        MenuOption(value: 2, text: "Web"),
-        MenuOption(value: 3, text: "backend"),
-        MenuOption(value: 4, text: "other")
-      ]);
-      final selectedOption = menu2.choose();
-      print('GREAT $selectedOption');
-      break;
-    case "no":
-      final menu3 = Menu<int>(label: "What is your favorite?", options: [
-        MenuOption(value: 1, text: "Series"),
-        MenuOption(value: 2, text: "Movies"),
-        MenuOption(value: 3, text: "Games"),
-        MenuOption(value: 4, text: "other")
-      ]);
-      print(menu3.choose());
-      break;
+  final loginResult = login("test@test.com", "test");
+  if (loginResult.left == 200) {
+    print("Token ${loginResult.right}");
+  } else if (loginResult == 404) {
+    print("user not found");
+  } else {
+    print("acces denied");
   }
+}
+
+Tuple<int, String?> login(String email, String password) {
+  if (email == "test@test.com" && password == "test") {
+    final token = "MY_TOKEN";
+    return Tuple(200, token);
+  } else if (email != "test@test.com") {
+    return Tuple(404, null);
+  } 
+  return Tuple(403, null);
 }
--- hello_world/lib/models/tuple.dart
+++ hello_world/lib/models/tuple.dart
@@ -1,3 +1,6 @@
 class Tuple<A, B> {
-  
+  final A left;
+  final B right;
+
+  Tuple(this.left, this.right);
 }
