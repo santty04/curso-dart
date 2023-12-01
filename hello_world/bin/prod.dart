f8722f1987f350478dd976275716c62a00d96251
santty04 <sarango844@misena.edu.co>
2023-11-30 20:33:16 -0500

    Futures

--- hello_world/bin/prod.dart
+++ hello_world/bin/prod.dart
@@ -1,4 +1,18 @@
-void main(List<String> arguments) {
-  print("prod mode");
-  print(arguments);
+import 'dart:async';
+
+void main(List<String> arguments) async {
+  print("hola");
+  final String value = await sleep(3000);
+  print(value);
+  print("😁");
+
+}
+
+Future<String> sleep(int milliseconds) {
+  Completer<String> completer = Completer();
+  
+  Timer(Duration(milliseconds: milliseconds), () {
+    completer.complete('done');
+  });
+  return completer.future;
 }
